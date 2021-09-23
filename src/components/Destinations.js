46d122f854eee98a05ac43c4d2d0946ff4acc96c
bophancungung <bophancungung@gmail.com>
2021-09-23 21:22:49 +0700

    update header

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,42 +1,42 @@
-import { useState, useContext } from 'react';
-import DestinationsContext from '../context/DestinationsContext';
-import DestinationsList from './DestinationsList';
+import { useState, useContext } from "react";
+import DestinationsContext from "../context/DestinationsContext";
+import DestinationsList from "./DestinationsList";
 const Destinations = () => {
-	const {
-		destinationsData: { destinations },
-	} = useContext(DestinationsContext);
+  const {
+    destinationsData: { destinations },
+  } = useContext(DestinationsContext);
 
-	const [state] = useState({
-		heading:
-			'Discover the most engaging places in the world with Travel Friends',
-		paragraph:
-			'Travel is a brutality. It forces you to trust strangers and to lose sight of all that familiar comforts of home and friends.You are constantly of balance. Nothing is yours except the essential things Air, Sleep, Dream, the sea, the sky-all things tending towards the eternal or what we imagine of it.',
-	});
-	return (
-		<div className='destinations'>
-			<div className='container'>
-				<div className='row ml-minus-15 mr-minus-15'>
-					<div className='col-6 p-15'>
-						<h3 className='destinations__heading animation'>{state.heading}</h3>
-					</div>
-					<div className='col-6 p-15'>
-						<p className='destinations__paragraph animation'>
-							{state.paragraph}
-						</p>
-					</div>
-				</div>
-				<div className='destinations__block'>
-					<div className='row ml-minus-15 mr-minus-15'>
-						{destinations.map((destination) => (
-							<DestinationsList
-								destination={destination}
-								key={destination.id}
-							/>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const [state] = useState({
+    heading:
+      "Mekong River are one of among strong and largest coconut producers and exporters in Vietnam with more than ten years experience.",
+    paragraph:
+      "We have factories in Ben Tre province, the main erea of planting and producing coconut of Vietnam, with well-equipped production technology and strict quality management, so our products can export to Europe, America, Midde East... with ISO 9001:2000, GMP, HACCP, HALAL, KOSHER, ORGANIC certification. Our daily capacity is 80 tons per day. Our office is located in Ho Chi Minh City, the main export centre of Vietnam. With these advantages, we're proud of fulfilling all your needs with best quality products and competative prices and stable supply. Our products are various from fresh coconut, desiccated coconut, coconut fibre, coconut charcoal, coconut oil, coconut milks and other products from coconut fibre…",
+  });
+  return (
+    <div className="destinations">
+      <div className="container">
+        <div className="row ml-minus-15 mr-minus-15">
+          <div className="col-6 p-15">
+            <h3 className="destinations__heading animation">{state.heading}</h3>
+          </div>
+          <div className="col-6 p-15">
+            <p className="destinations__paragraph animation">
+              {state.paragraph}
+            </p>
+          </div>
+        </div>
+        <div className="destinations__block">
+          <div className="row ml-minus-15 mr-minus-15">
+            {destinations.map((destination) => (
+              <DestinationsList
+                destination={destination}
+                key={destination.id}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Destinations;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,63 @@
-import { useState, useEffect, useContext } from 'react';
-import { Helmet } from 'react-helmet-async';
-import Header from '../components/Header';
-import Model from '../components/Model';
-import ModelContext from '../context/ModelContext';
-import { OPEN_MODEL } from '../context/types/ModelTypes';
-import Register from '../auth/Register';
-import Login from '../auth/Login';
-import Destinations from '../components/Destinations';
-import Services from '../components/services/Services';
-import Reviews from '../components/reviews/Reviews';
-import GalleryComponent from '../components/gallery/GalleryComponent';
-import Footer from '../components/footer/Footer';
+import { useState, useEffect, useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import Header from "../components/Header";
+import Model from "../components/Model";
+import ModelContext from "../context/ModelContext";
+import { OPEN_MODEL } from "../context/types/ModelTypes";
+import Register from "../auth/Register";
+import Login from "../auth/Login";
+import Destinations from "../components/Destinations";
+import Services from "../components/services/Services";
+import Reviews from "../components/reviews/Reviews";
+import GalleryComponent from "../components/gallery/GalleryComponent";
+import Footer from "../components/footer/Footer";
 const Home = () => {
-	const { dispatch } = useContext(ModelContext);
-	const [state] = useState({
-		heading: 'We are travel friends',
-		paragraph:
-			'Come and join us we travel the most famous and beautiful places in the world',
-	});
-	const [registerModel] = useState('registerModel');
-	const [loginModel] = useState('loginModel');
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
-	return (
-		<>
-			<Helmet>
-				<title>Travel Friends</title>
-				<meta
-					name='description'
-					content='Travel to the world with travel frirends'
-				/>
-				<meta name='keywords' content='travel, travel tours, airline' />
-			</Helmet>
-			<Header heading={state.heading} paragraph={state.paragraph}>
-				<button
-					className='btn-default'
-					onClick={() =>
-						dispatch({ type: OPEN_MODEL, payload: registerModel })
-					}>
-					Get Started
-				</button>
-			</Header>
-			<Model current={registerModel}>
-				<Register currentModel={loginModel} />
-			</Model>
-			<Model current={loginModel}>
-				<Login currentModel={registerModel} />
-			</Model>
-			<Destinations />
-			<Services />
-			<Reviews />
-			<GalleryComponent />
-			<Footer />
-		</>
-	);
+  const { dispatch } = useContext(ModelContext);
+  const [state] = useState({
+    heading0: "coconut mekong",
+    heading1: "products you love and trust",
+    paragraph:
+      "We're committed to providing the best customer service possible",
+  });
+  const [registerModel] = useState("registerModel");
+  const [loginModel] = useState("loginModel");
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      <Helmet>
+        <title>Coconut Mekong Vietnam</title>
+        <meta
+          name="description"
+          content="Travel to the world with travel frirends"
+        />
+        <meta name="keywords" content="travel, travel tours, airline" />
+      </Helmet>
+      <Header
+        heading0={state.heading0}
+        heading1={state.heading1}
+        paragraph={state.paragraph}
+      >
+        <button
+          className="btn-default"
+          onClick={() => dispatch({ type: OPEN_MODEL, payload: registerModel })}
+        >
+          More information
+        </button>
+      </Header>
+      <Model current={registerModel}>
+        <Register currentModel={loginModel} />
+      </Model>
+      <Model current={loginModel}>
+        <Login currentModel={registerModel} />
+      </Model>
+      <Destinations />
+      <Services />
+      <Reviews />
+      <GalleryComponent />
+      <Footer />
+    </>
+  );
 };
 export default Home;
